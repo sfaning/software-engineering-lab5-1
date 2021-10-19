79d12d00dda82189a5fc8f23bcf3f56e32bec181
ZhuoranWang <41529680+wang-zhuoran@users.noreply.github.com>
2021-10-19 09:54:29 +0800

    大概框架的建立

---
+++ oral_calculation.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <math.h>
+#include <set>
+using namespace std;
+
+int extractGrade(string &grade)
+{
+
+}
+
+double generateEquation(int grade)
+{
+
+}
+
+void read(string &grade, int &num_questions)
+{
+
+}
+
+void solve(string &grade, int &num_questions)
+{
+
+}
+
+int main()
+{
+    int num_questions;
+    string grade;
+
+    read(grade, num_questions);
+
+    solve(grade, num_questions);
+
+    system("pause");
+
+    return 0;
+}
